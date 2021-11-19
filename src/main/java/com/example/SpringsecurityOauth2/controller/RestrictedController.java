293e86ec1fcb58fc2d9e1e3ce0cfe1d6f867fea8
mzazakeith <mzazakeith@gmail.com>
2021-11-19 14:39:23 +0300

    principal accesss

--- src/main/java/com/example/SpringsecurityOauth2/controller/RestrictedController.java
+++ src/main/java/com/example/SpringsecurityOauth2/controller/RestrictedController.java
@@ -4,11 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping("restricted")
 public class RestrictedController {
     @GetMapping
     public ResponseEntity<String> restricted(){
         return new ResponseEntity<String>("You have been logged in successfully", HttpStatus.OK);
+    }
+
+    @GetMapping("user")
+    public Principal user(Principal principal){
+        return principal;
     }
 }
