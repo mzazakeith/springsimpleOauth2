8d592e714b096e235ecd5ff725b8743c3d46ec15
mzazakeith <mzazakeith@gmail.com>
2021-11-19 13:58:08 +0300

    web security configurations added

--- src/main/java/com/example/SpringsecurityOauth2/util/WebSecurityConfiguration.java
+++ src/main/java/com/example/SpringsecurityOauth2/util/WebSecurityConfiguration.java
@@ -1,4 +1,18 @@
 package com.example.SpringsecurityOauth2.util;
 
-public class WebSecurityConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**").authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+    }
 }
